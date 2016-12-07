3cb4db2de602a5c69de92ff6c34d5a777e18aa34
Vrungell <julia.kaliberda@gmail.com>
2016-12-08 01:33:30 +0700

    First

---
+++ Documents/Visual Studio 2013/Projects/NN2/NN2/Layer.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <vector>
+#include "neuron.h"
+class Layer
+{
+private:
+    std::vector<neuron> neurons;
+    int number_of_neurons;
+public:
+    Layer(int number_of_neurons, int number_of_weights);
+    int GetNumberOfNeurons();
+    std::vector<neuron> GetNeurons();
+    ~Layer();
+};
+

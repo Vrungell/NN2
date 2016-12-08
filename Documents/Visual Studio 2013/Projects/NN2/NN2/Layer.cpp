78e8ba6e1ddb10494a8823a165743812262c2d94
Vrungell <julia.kaliberda@gmail.com>
2016-12-09 01:02:59 +0700

    Second

--- Documents/Visual Studio 2013/Projects/NN2/NN2/Layer.cpp
+++ Documents/Visual Studio 2013/Projects/NN2/NN2/Layer.cpp
@@ -4,7 +4,7 @@
 
 Layer::Layer(int numberofneurons, int number_of_weights)
 {
-    number_of_neurons = numberofneurons;
+   this->number_of_neurons = numberofneurons;
     for (int i = 0; i < numberofneurons; i++){
         neuron neuron_for_adding(number_of_weights);
         neurons.push_back(neuron_for_adding);
@@ -12,11 +12,11 @@ Layer::Layer(int numberofneurons, int number_of_weights)
 }
 
 int Layer::GetNumberOfNeurons(){
-    return number_of_neurons;
+    return this->number_of_neurons;
 }
 
-std::vector<neuron> Layer::GetNeurons(){
-    return neurons;
+std::vector<neuron> &Layer::GetNeurons(){
+    return this->neurons;
 }
 
 Layer::~Layer()
